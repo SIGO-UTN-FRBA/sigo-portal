84818435b38dc7f6b453eef6bdb96846ee90b5ab
sergio087 <sergio.087@gmail.com>
2017-08-27 19:56:47 -0300

    Bootstrap's infraestructure to better, faster, stronger web development.

--- src/app/main/app.module.ts
+++ src/app/main/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './not-found.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HomeComponent} from "./home.component";
+import {NavbarModule} from "../navbar/navbar.module";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarModule
   ],
   exports: [],
   providers: [],
