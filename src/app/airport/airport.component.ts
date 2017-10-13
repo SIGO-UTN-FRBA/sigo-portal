1cbbfcb5c67e84c99956954224c815da45e70d3f
sergio087 <sergio.087@gmail.com>
2017-10-13 00:28:18 -0300

    Scroll to router outlet top when navigate to a route. #5

--- src/app/airport/airport.component.ts
+++ src/app/airport/airport.component.ts
@@ -2,8 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
   template: `
-    <router-outlet></router-outlet>
+    <router-outlet (activate)="onActivate($event, outlet)" #outlet></router-outlet>
   `
 })
 
-export class AirportComponent{}
+export class AirportComponent {
+  onActivate(e, outlet){
+    window.scrollTo(0, outlet.scrollTop);
+  }
+}
