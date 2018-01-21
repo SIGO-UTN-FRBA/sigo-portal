cb2bd0a270f71fe5496cc8911b08558acc2e97ef
Sergio Nascimbene <sergio.087@gmail.com>
2018-01-21 17:41:14 -0300

    Support handle TypeError and display friendly error message

--- src/app/commons/error-indicator.component.ts
+++ src/app/commons/error-indicator.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input} from '@angular/core';
 import {AppError} from "../main/ierror";
 
 @Component({
@@ -9,14 +9,22 @@ import {AppError} from "../main/ierror";
          class="alert alert-danger center-block -align-center"> 
       <span class="glyphicon glyphicon-alert"></span>
       <strong i18n="@@commons.alert.error">Error!</strong>
-      {{error.displayString()}}
+      {{displayError(error)}}
     </div>
   `
 })
 
 
-export class ErrorIndicatorComponent {
+export class ErrorIndicatorComponent{
 
   @Input() errors : AppError[];
 
+  displayError(error: any) : string {
+
+    if (error.name == "TypeError"){
+      console.error(error);
+      return "An expected error occurred on client app. Check console for more details..."
+    } else
+      return error.displayString();
+  }
 }
